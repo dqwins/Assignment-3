b1c4ee4aae68eca8bf061ee9ec8e13c0196a52f0
dqwins <darci.wright@uleth.ca>
2023-12-01 18:44:42 -0700

    ctrl s

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var x = document.title;
 console.log(x);
 
 
-$("#pencil").on().click(function () {   
+$("#pencil").on().click(function () {
     $("#pencil").css("transform", "translate(700px) rotate(-45deg)");
     $("#pencil").css("transition", "all 1s");
 });
