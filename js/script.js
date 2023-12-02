0019d1600b4e0a3eb6708c582ec681e482d5d7de
dqwins <darci.wright@uleth.ca>
2023-12-02 14:37:56 -0700

    trying to figue out how to hide man

--- js/script.js
+++ js/script.js
@@ -2,9 +2,18 @@ console.log("Connected!");
 var x = document.title;
 console.log(x);
 
+var man = $("#head");
 
 $("#pencil").on().click(function () {
     $("#pencil").css("transform", "translate(700px) rotate(-45deg)");
     $("#pencil").css("transition", "all 1s");
 });
+
+
+man.hide();
+
+
+//  $("#man-outline-01").on().click(function () {
+//     $(this).show();
+//  });
 
