a040f6e9b6e8f1004cb28cd6fc07c9edbf7daf57
dqwins <darci.wright@uleth.ca>
2023-12-04 16:06:36 -0700

    updated

--- js/script.js
+++ js/script.js
@@ -17,15 +17,22 @@ $("#Layer_1").on().click(function () {
     $("#Layer_1").hide();
 });
 
+$("#turn").on().click(function () {
+    $("#Layer_3").css("transform", "rotate(180deg)");
+    $("#Layer_3").css("transition", "all 1s");
+});
+
+
+
 
 //Coloring Settings
 
-$("#curvy-eyebrow1").on("click", function () {
-    $("#curvy-eyebrow1 .cls-1").css("fill", "#231c07");
+$("#eyedrow1").on("click", function () {
+    $("#eyedrow1 .cls-1").css("fill", "#231c07");
 });
 
-$("#curvy-eyebrow2").on("click", function () {
-    $("#curvy-eyebrow2 .cls-1").css("fill", "#231c07");
+$("#eyebrow2").on("click", function () {
+    $("#eyebrow2 .cls-1").css("fill", "#231c07");
 });
 
 $("#eyebrow3").on("click", function () {
@@ -38,13 +45,23 @@ $("#eyebrow4").on("click", function () {
 
 
 
-$("#hair1").on("click", function () {
-    $("hair1 .cls-1").css("fill", "pink");
+$("#tuff1").on("click", function () {
+    $("#tuff1 .cls-1").css("fill", "red");
 });
 
-$("#hair2").on("click", function () {
-    $("#hair2 .cls-1").css("fill", "#red");
+$("#tuff2").on("click", function () {
+    $("#tuff2 .cls-1").css("fill", "red");
 });
+
+$("#tuff3").on("click", function () {
+    $("#tuff3 .cls-1").css("fill", "red");
+});
+
+$("#tuff4").on("click", function () {
+    $("#tuff4 .cls-1").css("fill", "red");
+});
+
+
 
 $("#head").on("click", function () {
     $("#head .cls-1").css("fill", "pink");
@@ -52,9 +69,21 @@ $("#head").on("click", function () {
 
 
 
+$("#lefteye").on("click", function () {
+    $("#lefteye .cls-1").css("fill", "white");
+});
 
+$("#righteye").on("click", function () {
+    $("#righteye .cls-1").css("fill", "white");
+});
 
+$("#lefteye-pupil").on("click", function () {
+    $("#lefteye-pupil .cls-1").css("fill", "green");
+});
 
+$("#righteye-pupil").on("click", function () {
+    $("#righteye-pupil .cls-1").css("fill", "green");
+});
 
 
 
